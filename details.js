0d3c55088df92d7db7891f01226e3544e6ba45a9
djbenjamin2001 <benjaminbatbayli@gmail.com>
2022-09-21 10:54:00 +0200

    er færdig med den første fetch del af details, er i gang med at fetche casten

--- details.js
+++ details.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () =>{
            console.log(data)
            let hours = Math.floor(data.runtime/60)  
            let minutes = data.runtime % 60
-         let detailimage = document.createElement("div")
+         let detailimage = document.createElement("section")
          detailimage.innerHTML = `
    <a href="index.html"><i class="fa-solid fa-arrow-left img-arrow"></i> </a>
         <img class="backdrop__poster" src="${imgPath + data.backdrop_path}" alt="${imgPath + data.backdrop_path}">
@@ -17,17 +17,37 @@ document.addEventListener("DOMContentLoaded", () =>{
          detailhomepage.append(detailimage)
 
     let detailtextarea = document.createElement("section")
+    detailtextarea.classList.add("detailtextarea")
       detailtextarea.innerHTML =`
       <h2>${data.title}</h2>
-
-      <p><i class="fa-solid fa-star imdb-star"></i>${data.vote_average}/10 imdb</p>
+       <p><i class="fa-solid fa-star imdb-star"></i>${data.vote_average}/10 imdb</p>
+      <div class="detailgenre"></div>
       <p>${hours}h : ${minutes}min</p>
+      <p>${data.release_date}</p>
+      <div class="description">
      <h2>Description</h2>
      <p>${data.overview}</p>
+     </div>
       `
-
-
       detailhomepage.append(detailtextarea)
+
+let datagenreElm = document.querySelector(".detailgenre")
+console.log(datagenreElm)
+
+data.genres.forEach(genres => {
+  let genreData = document.createElement("p")
+  genreData.innerText = genres.name
+
+  datagenreElm.append(genreData)
+})
+      
     
+       })
+       fetch(`https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=2c8fc77b797cdf943cad63314a45daa4&language=en-US`)
+       .then(response => response.json())
+       .then(data =>{
+console.log(data)
+let detailcastSection = document.createElement("section")
+detailcastSection.classList.add("Castsection")
        })
     })
