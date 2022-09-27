6f26f1483280309e98dedae5cd64b3791024fe26
djbenjamin2001 <benjaminbatbayli@gmail.com>
2022-09-27 13:58:50 +0200

    infinite scrolling er indsættet og nu er siden færdig

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () =>{
    let wrapperElm = document.querySelector(".wrapper")
+   let popularPage = 1
     let nowshowing = document.querySelector(".nowshowing")
     let popular = document.querySelector(".popular")
     let footerwrapperElm = document.querySelector(".footerwrapper")
+
     let imgPath = "https://image.tmdb.org/t/p/original"
+
     let headerElm = document.createElement("header")
     headerElm.classList.add("header")
    headerElm.innerHTML = `
@@ -14,7 +17,8 @@ document.addEventListener("DOMContentLoaded", () =>{
 </label>
    `
    wrapperElm.append(headerElm)
-       fetch(" https://api.themoviedb.org/3/movie/popular?api_key=2c8fc77b797cdf943cad63314a45daa4&language=en-US&page=3")
+
+       fetch(" https://api.themoviedb.org/3/movie/popular?api_key=2c8fc77b797cdf943cad63314a45daa4&language=en-US&page=1")
       
        .then(response => response.json())
        .then(data =>{
@@ -50,27 +54,31 @@ nowshowing.append(nowshowingMovies)
          
        })
        //section 1 end
-       
-       fetch(" https://api.themoviedb.org/3/movie/popular?api_key=2c8fc77b797cdf943cad63314a45daa4&language=en-US&page=2")
+
+
+       let popularHeadline = document.createElement("div")
+       popularHeadline.classList.add("popularheadline")
+        popularHeadline.innerHTML = `<h2>Popular</h2>
+        <button>see more</button>
+        `
+      popular.append(popularHeadline)
+      
+       function fetchPopular(page){
+       fetch(`https://api.themoviedb.org/3/movie/popular?api_key=2c8fc77b797cdf943cad63314a45daa4&page=${page}`)
        .then(response => response.json())
        .then(data =>{
         let imgPathPopular = "https://image.tmdb.org/t/p/original"
            console.log(data)
           
-           let popularHeadline = document.createElement("div")
-           popularHeadline.classList.add("popularheadline")
-            popularHeadline.innerHTML = `<h2>Popular</h2>
-            <button>see more</button>
-            `
-          popular.append(popularHeadline)
+         
          
           let popularMovies = document.createElement("div")
           popularMovies.classList.add("showingpopular")
           popular.append(popularMovies)
 
 
-          data.results.forEach(movie => {
-            let showingpopular = document.createElement("section")
+          data.results.forEach((movie, index) => {
+            let showingpopular = document.createElement("article")
            showingpopular.classList.add("popularmovies")
         showingpopular.innerHTML = `
         <a href="detail.html?id=${movie.id}"><img class="poster--popular" src="${imgPathPopular + movie.poster_path}" alt=""></a>
@@ -91,21 +99,41 @@ movie.genre_ids.forEach(id =>{
    genreSpan.classList.add("genre__tag")
    genreSpan.innerText = currentGenre.name
    genreElm.append(genreSpan)
+
 })
 
+if (index === 18){
+   const intersectionObserver = new IntersectionObserver((entries) => {
+      // If intersectionRatio is 0, the target is out of view
+      // and we do not need to do anything.
+      if (entries[0].intersectionRatio <= 0) return;
+    
+     
+      popularPage++;
+      console.log('items in viewport');
+      fetchPopular(popularPage)
+      intersectionObserver.unobserve(showingpopular)
+     
+    });
+    intersectionObserver.observe(showingpopular);
+}
 
              })
-             let footerElm = document.createElement("footer")
-             footerElm.classList.add("footer")
-             footerElm.innerHTML = `
-             <i class="fa-solid fa-bookmark"></i>
-             <i class="fa-solid fa-ticket"></i>
-             <i class="fa-solid fa-film"></i>
-             `
-              footerwrapperElm.append(footerElm)
+             
+            
      
            })
          
-         
+         }
+         fetchPopular(popularPage)
+         let footerElm = document.createElement("footer")
+         footerElm.classList.add("footer")
+         footerElm.innerHTML = `
+         <i class="fa-solid fa-bookmark"></i>
+         <i class="fa-solid fa-ticket"></i>
+         <i class="fa-solid fa-film"></i>
+         `
+          footerwrapperElm.append(footerElm)
+
        })
-     
+      
